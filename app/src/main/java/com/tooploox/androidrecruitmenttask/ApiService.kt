9c34fa4de65e1c01cd152a39d712eb87548406e7
Łukasz Kasprzyk <lukasz.kasprzyk@tooploox.com>
2022-11-09 10:34:54 +0100

    Update retrofit service

--- app/src/main/java/com/tooploox/androidrecruitmenttask/ApiService.kt
+++ app/src/main/java/com/tooploox/androidrecruitmenttask/ApiService.kt
@@ -1,5 +1,24 @@
 package com.tooploox.androidrecruitmenttask
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Retrofit
+
 class ApiService {
-    // Implement Retrofit service
+    
+    private val BASE_URL = "https://itunes.apple.com/"
+
+    private val contentType = MediaType.get("application/json")
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @ExperimentalSerializationApi
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(
+            json.asConverterFactory(contentType)
+        )
+        .build()
 }
